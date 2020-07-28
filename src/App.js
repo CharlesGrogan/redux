905fa3b57bad666bbe3a7be706f3daea63a2354f
CGROGAN <grogan.charlie@gmail.com>
2020-07-28 18:18:18 -0500

    redux added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 import './App.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="wrapper">
       <Postform />
       <hr />
       <Posts />
     </div>
+    </Provider>
   );
 }
 
